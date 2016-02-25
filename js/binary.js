af298ed4d6ec973f5fea93e7c29fbff3d3b50b22
Jun Bon Kok <junbon@binary.com>
2016-02-25 14:30:00 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -62480,6 +62480,8 @@ function contractTypeDisplayMapping(type) {
         DIGITUNDER: "bottom",
         EXPIRYRANGE: "top",
         EXPIRYMISS: "bottom",
+        EXPIRYRANGEE: "top",
+        EXPIRYMISSE: "bottom",
         RANGE: "top",
         UPORDOWN: "bottom",
         ONETOUCH: "top",
