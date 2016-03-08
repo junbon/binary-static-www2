243e7f5d9dbda22bf4f525f21df2078874a75660
Jun Bon Kok <junbon@binary.com>
2016-03-08 17:25:20 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -70175,9 +70175,20 @@ function attach_tabs(element) {
 		details:{
 			value:function(data){
 				var active_symbols = [];
+                                var allowed_symbols = {
+                                    frxAUDJPY: 1,
+                                    frxAUDUSD: 1,
+                                    frxEURGBP: 1,
+                                    frxEURJPY: 1,
+                                    frxEURUSD: 1,
+                                    frxGBPJPY: 1,
+                                    frxGBPUSD: 1,
+                                    frxUSDCAD: 1,
+                                    frxUSDJPY: 1
+                                };
 
 				data.active_symbols.forEach(function(symbol){
-					if(symbol.market==='forex' && symbol.submarket==='major_pairs'){
+					if(typeof allowed_symbols[symbol.symbol] !== 'undefined'){
 						active_symbols.push(symbol);
 					}
 				});
