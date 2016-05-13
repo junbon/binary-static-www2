cec774ec1fbc87409b60a1213c66d6c91cf15d7e
binary-hamedanchi <hamedanchi@gmail.com>
2016-05-13 11:34:18 +0800

    Remove template copying

--- build/copy.js
+++ build/copy.js
@@ -6,7 +6,6 @@ module.exports = {
             { expand: true, cwd: 'src/config/', src: ['**'], dest: 'dist/config/' },
             { expand: true, cwd: 'src/images/', src: ['**'], dest: 'dist/images/', },
             { expand: true, cwd: 'src/download/', src: ['**'], dest: 'dist/download/' },
-            { expand: true, cwd: 'src/templates/', src: ['**'], dest: 'dist/templates/' },
             { expand: true, cwd: 'src/css/external/jquery-ui-custom-theme/images/', src: ['**'], dest: 'dist/css/images' },
             { expand: true, cwd: 'src/css/external/jquery-ui-custom-theme/', src: ['*.css'], dest: 'dist/css/' },
             { expand: true, cwd: 'src/javascript/', src: ['**'], dest: 'dist/dev/src/javascript/' },
